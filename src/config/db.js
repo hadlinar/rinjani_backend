f0d4cc120abe5af1f195c80c278fc40c3615feed
Hadlina Rahmadinni <hadlinar@gmail.com>
2022-03-28 09:08:44 +0700

    change db host

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,10 @@
 const Pool = require('pg').Pool
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: '170.1.70.57',
-//   database: 'krakatau',
-//   password: 'nusindopub2021**',
-//   port: 5432,
-// })
-
 const pool = new Pool({
-  user: 'postgre',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
+  user: 'postgres',
+  host: '170.1.70.57',
+  database: 'krakatau',
+  password: 'nusindopub2021**',
   port: 5432,
 })
 
